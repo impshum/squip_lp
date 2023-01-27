ec1d94e07b9939c5afcb4ca271beab70f86f4c91
dmirakyan <damirakyan@gmail.com>
2023-01-27 15:26:17 -0300

    refreshed lp

---
+++ public/js/main.js
@@ -0,0 +1,115 @@
+// global object
+const YOU = {};
+
+// init slider
+YOU.initSlider = (selector = ".carousel__wrapper", options = {}) => {
+  const containers = Array.from(document.querySelectorAll(selector));
+
+  if (!containers.length) return;
+
+  containers.forEach(function (container) {
+    const splide = new Splide(container, options);
+
+    splide.mount();
+  });
+};
+
+// manage carousels
+YOU.manageCarousels = () => {
+  YOU.initSlider("#testimonialSlider", {
+    type: "loop",
+    perPage: 2,
+    pagination: false,
+    arrows: false,
+    focus: "center",
+    updateOnMove: true,
+    padding: {
+      left: "20%",
+      right: "20%",
+    },
+    breakpoints: {
+      767: {
+        perPage: 1,
+      },
+    },
+  });
+};
+
+// type writer
+YOU.createTypeWriter = function () {
+  // get the element
+  const textFields = Array.from(document.querySelectorAll(".typing-text"));
+
+  if (!textFields.length) return;
+
+  // make a words array
+  const words = ["Quality", "Operations", "Innovation", "Compliance"];
+
+  textFields.forEach(function (text) {
+    const data = text.dataset.content || text.getAttribute("data-content");
+    const content = data ? data.split(",") : words;
+
+    // start typing effect
+    setTyper(text, content);
+  });
+
+  function setTyper(element, words) {
+    const LETTER_TYPE_DELAY = 75;
+    const WORD_STAY_DELAY = 2000;
+
+    const DIRECTION_FORWARDS = 0;
+    const DIRECTION_BACKWARDS = 1;
+
+    var direction = DIRECTION_FORWARDS;
+    var wordIndex = 0;
+    var letterIndex = 0;
+
+    var wordTypeInterval;
+
+    startTyping();
+
+    function startTyping() {
+      wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+    }
+
+    function typeLetter() {
+      const word = words[wordIndex];
+
+      if (direction == DIRECTION_FORWARDS) {
+        letterIndex++;
+
+        if (letterIndex == word.length) {
+          direction = DIRECTION_BACKWARDS;
+          clearInterval(wordTypeInterval);
+          setTimeout(startTyping, WORD_STAY_DELAY);
+        }
+      } else if (direction == DIRECTION_BACKWARDS) {
+        letterIndex--;
+
+        if (letterIndex == 0) {
+          nextWord();
+        }
+      }
+
+      const textToType = word.substring(0, letterIndex);
+
+      element.textContent = textToType;
+    }
+
+    function nextWord() {
+      letterIndex = 0;
+      direction = DIRECTION_FORWARDS;
+      wordIndex++;
+
+      if (wordIndex == words.length) {
+        wordIndex = 0;
+      }
+    }
+  }
+};
+
+// document on load
+document.addEventListener("DOMContentLoaded", () => {
+  YOU.manageCarousels();
+  YOU.createTypeWriter();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,30 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ["./**/*.html"],
   theme: {
-    fontSize: {
-      xs: ['0.75rem', { lineHeight: '1rem' }],
-      sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '1.75rem' }],
-      lg: ['1.125rem', { lineHeight: '2rem' }],
-      xl: ['1.25rem', { lineHeight: '2rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2rem' }],
-      '3xl': ['2rem', { lineHeight: '2.5rem' }],
-      '4xl': ['2.5rem', { lineHeight: '3.5rem' }],
-      '5xl': ['3rem', { lineHeight: '3.5rem' }],
-      '6xl': ['3.75rem', { lineHeight: '1' }],
-      '7xl': ['4.5rem', { lineHeight: '1.1' }],
-      '8xl': ['6rem', { lineHeight: '1' }],
-      '9xl': ['8rem', { lineHeight: '1' }],
-    },
     extend: {
-      borderRadius: {
-        '4xl': '2rem',
+      screens: {
+        sm: "576px",
+        xs: "420px",
+      },
+      container: {
+        center: true,
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        display: ['Lexend', ...defaultTheme.fontFamily.sans],
+        display: ["Tinos", "serif"],
+        body: ["Inter", "sans-serif"],
+        sfpro: ["'SF Pro'", "sans-serif"],
       },
-      maxWidth: {
-        '2xl': '40rem',
+      colors: {
+        red: {
+          1000: "#410B13",
+          1100: "#FF5658",
+        },
+        dark: {
+          500: "#270006",
+        },
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
-}
+  plugins: [],
+};
